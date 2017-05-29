a8869475c5aa2745c3dd097d1415de0d5cf8f012
Cblpka <vehluncho@gmail.com>
2017-05-29 13:43:51 +0300

    Initial Commit

---
+++ step_test7/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  step_test7
+//
+//  Created by Admin on 29.05.17.
+//  Copyright © 2017 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
